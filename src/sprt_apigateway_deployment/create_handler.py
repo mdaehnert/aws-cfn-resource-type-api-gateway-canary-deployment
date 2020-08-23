28f7f139083697d2426d8a033b1e336aeb3ea4a1
Christian Koops <20813198+koopsc@users.noreply.github.com>
2020-08-24 00:58:02 +0200

    explicite error handling for NotFound upon create

--- src/sprt_apigateway_deployment/create_handler.py
+++ src/sprt_apigateway_deployment/create_handler.py
@@ -1,6 +1,6 @@
 from typing import MutableMapping, Any
 
-from cloudformation_cli_python_lib import OperationStatus, ProgressEvent
+from cloudformation_cli_python_lib import OperationStatus, ProgressEvent, HandlerErrorCode
 from mypy_boto3_apigateway.client import APIGatewayClient
 
 from .models import ResourceModel
@@ -10,26 +10,31 @@ CREATED = "CREATED"
 
 def handle_create(agw_client: APIGatewayClient, model: ResourceModel,
                   callback_context: MutableMapping[str, Any]) -> ProgressEvent:
-    progress = ProgressEvent(status=OperationStatus.IN_PROGRESS)
-
     if callback_context.get("green-status") == CREATED:
         stage_name = "blue"
         final_state = OperationStatus.SUCCESS
     else:
         stage_name = "green"
         final_state = OperationStatus.IN_PROGRESS
-
-    agw_client.create_stage(
-        restApiId=model.RestApiId,
-        stageName=stage_name,
-        deploymentId=model.DeploymentId,
-        description=f"{stage_name} production stage",
-        cacheClusterEnabled=False,
-        tracingEnabled=model.TracingEnabled
-    )
+    try:
+        agw_client.create_stage(
+            restApiId=model.RestApiId,
+            stageName=stage_name,
+            deploymentId=model.DeploymentId,
+            description=f"{stage_name} production stage",
+            cacheClusterEnabled=False,
+            tracingEnabled=model.TracingEnabled
+        )
+    except agw_client.exceptions.NotFoundException:
+        return ProgressEvent(
+            status=OperationStatus.FAILED,
+            errorCode=HandlerErrorCode.NotFound,
+            message="REST API does not exist"
+        )
     model.StageName = "blue"
     callback_context[f"{stage_name}-status"] = CREATED
-    progress.callbackContext = callback_context
-    progress.status = final_state
-    progress.resourceModel = model
-    return progress
+    return ProgressEvent(
+        status=final_state,
+        callbackContext=callback_context,
+        resourceModel=model,
+    )
