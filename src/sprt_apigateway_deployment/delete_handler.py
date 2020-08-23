a2e31d9eb23a42f78011ed7396a0ad69a7a64803
Christian Koops <20813198+koopsc@users.noreply.github.com>
2020-08-24 01:11:56 +0200

    fix contract on delete

--- src/sprt_apigateway_deployment/delete_handler.py
+++ src/sprt_apigateway_deployment/delete_handler.py
@@ -10,23 +10,22 @@ DELETED = "DELETED"
 
 def handle_delete(agw_client: APIGatewayClient, model: ResourceModel,
                   callback_context: MutableMapping[str, Any]) -> ProgressEvent:
-    progress = ProgressEvent(
-        status=OperationStatus.IN_PROGRESS,
-        resourceModel=model,
-    )
-
-    if callback_context.get("blue-status") == DELETED:
-        stage_name = "green"
-        final_state = OperationStatus.SUCCESS
+    if callback_context.get("blue-status") is None:
+        agw_client.delete_stage(
+            restApiId=model.RestApiId,
+            stageName="blue"
+        )
+        callback_context["blue-status"] = DELETED
+        return ProgressEvent(
+            status=OperationStatus.IN_PROGRESS,
+            resourceModel=model,
+            callbackContext=callback_context,
+        )
     else:
-        stage_name = "blue"
-        final_state = OperationStatus.IN_PROGRESS
-
-    agw_client.delete_stage(
-        restApiId=model.RestApiId,
-        stageName=stage_name
-    )
-    callback_context[f"{stage_name}-status"] = DELETED
-    progress.callbackContext = callback_context
-    progress.status = final_state
-    return progress
+        agw_client.delete_stage(
+            restApiId=model.RestApiId,
+            stageName="green"
+        )
+        return ProgressEvent(
+            status=OperationStatus.SUCCESS,
+        )
